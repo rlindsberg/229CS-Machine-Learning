a555550248977f76e6a15249c48df0f1eff9414a
rlindsberg <roderick@karlemstrand.com>
2019-04-20 09:41:16 +0200

    done One-vs-All Classifier Prediction

--- ex3/src/predictOneVsAll.m
+++ ex3/src/predictOneVsAll.m
@@ -1,17 +1,17 @@
 function p = predictOneVsAll(all_theta, X)
-%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
-%are in the range 1..K, where K = size(all_theta, 1). 
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels
+%are in the range 1..K, where K = size(all_theta, 1).
 %  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
 %  for each example in the matrix X. Note that X contains the examples in
 %  rows. all_theta is a matrix where the i-th row is a trained logistic
 %  regression theta vector for the i-th class. You should set p to a vector
 %  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
-%  for 4 examples) 
+%  for 4 examples)
 
 m = size(X, 1);
 num_labels = size(all_theta, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
@@ -24,15 +24,23 @@ X = [ones(m, 1) X];
 %               num_labels).
 %
 % Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       In particular, the max function can also return the index of the
+%       max element, for more information see 'help max'. If your examples
+%       are in rows, then, you can use max(A, [], 2) to obtain the max
 %       for each row.
-%       
+%
 
+% op1 is 5000x401, op2 is 10x401
+% pred is hθ(x), a 5000 x 10 matrix
+pred = sigmoid(X * all_theta')
 
+% t.ex pred(100,:) ==
+% 7.2507e-18   1.0876e-02   4.0217e-05   3.7104e-09   1.0606e-06   2.3666e-05   2.8987e-04   7.4201e-05   5.4682e-03   9.9998e-01
+% >> [M, I] = max(pred(100,:))
+% M =  0.99998
+% I =  10
 
-
+[~, p] = max(pred, [], 2)
 
 
 
