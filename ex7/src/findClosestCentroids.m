93d42411887a791ee1bfcc890339c30d1e50d48e
rlindsberg <roderick@karlemstrand.com>
2019-06-20 22:41:55 +0200

    Iterate, find the min distance and return the closest centroid with bug

--- ex7/src/findClosestCentroids.m
+++ ex7/src/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,7 +15,7 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
@@ -23,6 +23,30 @@ idx = zeros(size(X,1), 1);
 
 
 
+shortestPath = 0;
+
+for i = 1:3
+    sprintf('Testing %d', i)
+
+    shortestPathSquared = (X(i) - centroids(1))^2 + (X(1) - centroids(1))^2;
+    shortestPath = sqrt(shortestPathSquared);
+    idx(i) = 1;
+    sprintf('shortestPath is %f\n', shortestPath)
+
+
+    for j = 1:size(centroids)
+        sprintf('\n Inner loop begins\n')
+        distanceSquared = (X(i) - centroids(j))^2 + (X(1) - centroids(1))^2;
+        distance = sqrt(distanceSquared);
+        sprintf('The distance between X(%d) and centroids(%d) is %f', i, j, distance)
+        if distance < shortestPath
+            sprintf('\n******** Found a shorter path!!!\n')
+            shortestPath = distance
+            sprintf('********\n\n')
+            idx(i) = j;
+        endif
+    end
+end
 
 
 
@@ -30,4 +54,3 @@ idx = zeros(size(X,1), 1);
 % =============================================================
 
 end
-
